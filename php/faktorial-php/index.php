1ff76a06f308e3c2a90c49ea6be04f2368120679
meshosk <meshosk@gmail.com>
2021-05-19 12:23:18 +0200

    Upravenie startovacieho stavu

--- php/faktorial-php/index.php
+++ php/faktorial-php/index.php
@@ -1,19 +1,4 @@
-<?php
-
-function fakt($cislo){
-    $vysledok = 1;
-    while (--$cislo  > 0 ) {
-        $vysledok *= $cislo+1;
-    }
-    return $vysledok;
-}
-?>
 <html>
     <body>
-        <ul>
-            <?php for ($i = 0; $i < 10; $i++) { ?>
-            <li><?php echo $i . "! = ". fakt($i)?></li>
-            <?php } ?>
-        </ul>
     </body>
 </html>
